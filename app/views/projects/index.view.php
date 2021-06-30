fc1101b23c3033bc44b389e4b913b27e8ae99b0f
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2021-06-30 21:47:23 +0800

    fix deleting project and added n+1 solver

--- app/views/projects/index.view.php
+++ app/views/projects/index.view.php
@@ -7,7 +7,6 @@ require __DIR__ . '/../layouts/head.php'; ?>
 <div class="col-md-12 pb-3">
 	<a href="<?= route('/project/add') ?>" class="btn btn-success btn-sm text-rigth"><i class="fas fa-plus"></i> Add Project</a>
 </div>
-
 <div class="pb-3">
 	<div class="col">
 		<?= msg('RESPONSE_MSG'); ?>
@@ -36,11 +35,11 @@ require __DIR__ . '/../layouts/head.php'; ?>
 									<a class="text-muted mr-2" href="<?= route('/project/detail', $projects['id']) ?>" style="font-size: 18px;" data-toggle="tooltip" data-placement="bottom" title="Edit">
 										<i class="far fa-edit"></i>
 									</a>
-									<a class="text-muted mr-2" href="<?= route('/project/delete', $projects['id']) ?>" style="font-size: 18px;" data-toggle="tooltip" data-placement="bottom" title="Delete" onclick="event.preventDefault(); document.getElementById('delete-project-form').submit();">
+									<a class="text-muted mr-2" href="<?= route('/project/delete', $projects['id']) ?>" style="font-size: 18px;" data-toggle="tooltip" data-placement="bottom" title="Delete" onclick="event.preventDefault(); document.getElementById('delete-project-form-<?= $projects['id'] ?>').submit();">
 										<i class="far fa-trash-alt"></i>
 									</a>
 
-									<form id="delete-project-form" action="<?= route('/project/delete', $projects['id']) ?>" method="POST" style="display:none;">
+									<form id="delete-project-form-<?= $projects['id'] ?>" action="<?= route('/project/delete', $projects['id']) ?>" method="POST" style="display:none;">
 										<?= csrf() ?>
 									</form>
 								</td>
